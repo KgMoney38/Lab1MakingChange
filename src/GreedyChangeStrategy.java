8fa01f1c446271b6be68c50394125dd5dc864571
KgMoney38 <kodydavison@hotmail.com>
2025-04-17 16:19:17 -0500

    also finished this one today. It uses my algorithm to always use the largest domination first

--- src/GreedyChangeStrategy.java
+++ src/GreedyChangeStrategy.java
@@ -1,13 +1,17 @@
 import java.util.List;
 
+//This class makes change using my greedy algorithm strategy
+//Basically always use the largest denomination possible first and continue tha pattern until the final amount is reached
 public class GreedyChangeStrategy implements ChangeStrategy
 {
+    //List of my denominations
     private final List<Denomination> denominations;
 
     public GreedyChangeStrategy(List<Denomination> denominations) {
         this.denominations = denominations;
     }
 
+    //Returns the change using the fewest number of bills and coins
     @Override
     public Purse makeChange(double amt) {
         Purse purse = new Purse();
