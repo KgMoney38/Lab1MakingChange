d315663fb695d8a96fb0f033ed71af34743a30ea
KgMoney38 <kodydavison@hotmail.com>
2025-04-17 16:15:36 -0500

    Didnt change much mostly added comments here as well

--- src/PursePanel.java
+++ src/PursePanel.java
@@ -1,24 +1,50 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Map;
 
+//Displays the contents of the purse using my images
 public class PursePanel extends JPanel implements PurseObserver {
-    private JTextArea display;
+    private JPanel coinDisplay;
 
+    //Set up my display panel and registers as an observer
     public PursePanel(Purse purse) {
-        purse.addObserver(this); // Register this panel as an observer
-
-        display = new JTextArea(10, 40);
-        display.setEditable(false);
-
+        purse.addObserver(this);
         setLayout(new BorderLayout());
-        add(new JScrollPane(display), BorderLayout.CENTER);
 
-        // Initial state
-        update(purse);
+        coinDisplay = new JPanel();
+        coinDisplay.setLayout(new GridLayout(0, 2, 10, 10)); // two columns
+
+        add(new JScrollPane(coinDisplay), BorderLayout.CENTER);
+        update(purse); // first draw
     }
 
+    //Only called when the purse updates it updates the coin display
     @Override
     public void update(Purse purse) {
-        display.setText(purse.toString());
+        coinDisplay.removeAll();
+
+        Map<Denomination, Integer> contents = purse.getCash();
+
+        contents.entrySet()
+                .stream()
+                .sorted((a, b) -> Double.compare(b.getKey().amt(), a.getKey().amt())) // descending
+                .forEach(entry -> {
+                    Denomination d = entry.getKey();
+                    int count = entry.getValue();
+
+                    String iconPath = "/images/" + d.iconName() + ".png";
+                    ImageIcon originalIcon = new ImageIcon(getClass().getResource(iconPath));
+                    Image scaledImage = originalIcon.getImage().getScaledInstance(100, 60, Image.SCALE_SMOOTH);
+                    ImageIcon resizedIcon = new ImageIcon(scaledImage);
+
+                    JLabel label = new JLabel(count + " x " + d.name(), resizedIcon, JLabel.LEFT);
+                    label.setHorizontalTextPosition(JLabel.RIGHT);
+                    label.setIconTextGap(10);
+
+                    coinDisplay.add(label);
+                });
+
+        revalidate();
+        repaint();
     }
 }
