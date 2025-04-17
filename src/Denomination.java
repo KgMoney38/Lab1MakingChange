72eb92d42a6b1f0ca6cf200eef5035302d66acc0
KgMoney38 <kodydavison@hotmail.com>
2025-04-16 22:36:19 -0500

    Final basic image path class just reused Denomination

--- src/Denomination.java
+++ src/Denomination.java
@@ -1,10 +1,9 @@
-public record Denomination(String name,double amt, String form, String img)
-{
-    //Done.
-    //Note: Records automatically create many functions for the final variables above.
-    //Final Variables description below
-    //name: String // name of the denomination. E.g. Ten Note, Five Note, Dime, Nickle, etc.
-    //amt: double // The amount of money this denomination represents
-    //form: String // “bill” or “coin”; This could be an enum, if you know them.
-    //img: String // The string containing the image name for an image of the type of money
+//Updated to just return my image file name
+
+public record Denomination(String name, double amt, String form, String img) {
+
+    //This is what actually returns the path
+    public String iconName() {
+        return img;
+    }
 }
