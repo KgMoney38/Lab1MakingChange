515bd8d745c843f5cbaf0a672404c434dd5e8d5a
KgMoney38 <kodydavison@hotmail.com>
2025-04-17 16:17:46 -0500

    had to update this one to follow a more concise change making strategy

--- src/Register.java
+++ src/Register.java
@@ -1,12 +1,15 @@
 import java.util.List;
 
+//The strategy used to make change
 public class Register {
     private final ChangeStrategy strategy;
 
+    //Sets the strategy for making change
     public Register(ChangeStrategy strategy) {
         this.strategy = strategy;
     }
 
+    //Returns the change for the amount entered
     public Purse makeChange(double amount) {
         return strategy.makeChange(amount);
     }
