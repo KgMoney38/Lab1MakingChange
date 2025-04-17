6e5734274151eedce13782b789d313de5e29d3b0
KgMoney38 <kodydavison@hotmail.com>
2025-04-16 22:33:15 -0500

    Final Strategy interface implementation

--- src/ChangeStrategy.java
+++ src/ChangeStrategy.java
@@ -1,4 +1,7 @@
+//NEW for my lab 4
+//Strategy interface for creating the change from the dollar amount entered
 public interface ChangeStrategy
 {
+    //Returns the purse with the change amount
     Purse makeChange(double amount);
 }
